e7c986d3a56929a9e7f39044af2c48a28fd814bb
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-04-21 17:05:55 +0300

    Fix typo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/v1/": "https://mern-ai-szmc.onrender.com",
+      "/api/v1/": "https://mern-ai-szmc.onrender.com/",
     },
   },
 })
